45831de0e296ccadd0d02e82271c074911defa7e
Chris Curl <curlc@yahoo.com>
2025-07-17 02:02:24 -0400

    Fix / Cleanup

--- heap.h
+++ heap.h
@@ -5,14 +5,20 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// Change these to control heap size and index
-// For details, see HEAP_T in heap.c
+// Change these to control the heap and index sizes.
+// Or, they can be set before #including this file.
+// For details, see heap.c
 
+#ifndef HEAPINDEX_SZ
 #define HEAPINDEX_SZ 500
+#endif
+
+#ifndef HEAP_SZ
 #define HEAP_SZ 10000
+#endif
 
 extern char *hAlloc(int sz);
 extern void hFree(char *data);
-extern void hDump();
+void hDump(int details, FILE *toFP);
 
 #endif // __HALLOC__
--- tc.h
+++ tc.h
@@ -6,19 +6,24 @@
 #include <string.h>
 #include "heap.h"
 
+#define SYMBOLS_SZ   500
+#define CODE_SZ     2500
+#define NODES_SZ    1000
+
+#define BTWI(n,l,h) ((l<=n)&&(n<=h))
+
 typedef unsigned char byte;
 typedef byte code;
 
 typedef struct { char type, *name; long val; } SYM_T;
-extern SYM_T symbols[];
+extern SYM_T symbols[SYMBOLS_SZ];
 
 enum {
     NOP, IFETCH, ISTORE, IP1, IP2, IP4, IDROP, IADD, ISUB, IMUL, IDIV,
     ILT, IGT, IEQ, JZ, JNZ, JMP, ICALL, IRET, HALT
 };
 
-extern long globals[];
-extern code vm[];
+extern code vm[CODE_SZ];
 extern void initVM();
 extern void runVM(int pc);
 extern void dis(int here);
