70428f1e486f0c10b854626514b457c3d064272a
Chris Curl <curlc@yahoo.com>
2025-08-04 08:24:05 -0400

    more

--- xx.asm
+++ xx.asm
@@ -2,10 +2,6 @@
 
 format ELF executable
 
-segment readable writable
-
-; nothing here
-
 segment readable executable
 
 ; -------------------------------------------------------------------------------------
@@ -14,6 +10,17 @@ segment readable executable
 entry _start
 
 _start:
+    mov eax,0x41414141
+
     mov eax, 1      ; syscall number for exit
-    xor ebx, ebx    ; exit code 0
+    mov ebx, 22     ; exit code 22
     int 0x80        ; call kernel
+
+    mov eax,0x42424242
+    lea ecx, [xmsg]
+    mov eax,0x5A5A5A5A
+
+segment readable writable
+xaaa: db "aaaaaaaa",0
+xmsg: db "hi there",0
+xzzz: db "zzzzzzzz",0
