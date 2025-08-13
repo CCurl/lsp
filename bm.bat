2108b3600a8ca068bca6dcdb8a9efed077302293
Chris Curl <ccurl@solventum.com>
2025-08-13 14:06:18 -0400

    more

---
+++ bm.bat
@@ -0,0 +1,14 @@
+@echo off
+rem this is a stupid little batch file to help test the code on windows machines
+
+set f=%1%
+
+if [%f%] equ [] (
+	set %f=debug
+)
+
+echo f=%f%
+
+%f%\tc bm.tc
+%f%\vm
+%f%\hex-dump tc.out >> vm.lst
